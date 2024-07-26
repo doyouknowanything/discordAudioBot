e2e642a5a69242a0343ba39808c3d624e27c36a6
Andreev Ilya <andreevkorn@yandex.ru>
2024-07-26 16:23:13 +0300

    init commit, simple working bot done

---
+++ src/AudioBot/main.cpp
@@ -0,0 +1,25 @@
+#include <dpp/dpp.h>
+#include <iostream>
+
+constexpr char * BOT_TOKEN = "TOKEN";
+
+
+int main() {
+    dpp::cluster bot(BOT_TOKEN);
+
+    bot.on_log(dpp::utility::cout_logger());
+
+    bot.on_slashcommand([](const dpp::slashcommand_t& event) {
+        if (event.command.get_command_name() == "ping") {
+            event.reply("Понг блять");
+        }
+    });
+
+    bot.on_ready([&bot](const dpp::ready_t& event) {
+        if (dpp::run_once<struct register_bot_commands>()) {
+            bot.global_command_create(dpp::slashcommand("ping", "Ping pong!", bot.me.id));
+        }
+    });
+
+    bot.start(dpp::st_wait);
+}
